2981aac904ab5ee739d6b9c68d48871207f07b83
Ekaterina Bobyleva <129331488+bobylevaea@users.noreply.github.com>
2023-07-14 23:58:51 +0300

    Added solutions tasks #40, 42

--- home_work_09/tasks.py
+++ home_work_09/tasks.py
@@ -1,1 +1,21 @@
 
+import pandas as pd 
+  
+# Загрузка данных из файла 
+df = pd.read_csv('/content/sample_data/california_housing_train.csv') 
+  
+# Задача 40: Работать с файлом california_housing_train.csv, который находится в папке 
+# sample_data. Определить среднюю стоимость дома, где кол-во людей от 0 до 500 (population)
+ 
+mean_house_value = round(df[df['population'] < 501]['median_house_value'].mean(), 2) 
+print (f'Средняя стоимость дома: {mean_house_value}')
+
+# mean    206799.95 
+
+  
+# Задача 42: Узнать какая максимальная households в зоне минимального значения population 
+
+max_households = df[df['population'] == df['population'].min()]['households'].max()
+print(f'Максимальная households: {max_households}')
+
+# max    4.0
