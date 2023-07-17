eb2a978ce10763119d5029dc9ce5e337df169517
Ekaterina Bobyleva <129331488+bobylevaea@users.noreply.github.com>
2023-07-17 18:56:25 +0300

    Added solution task #44

---
+++ home_work_10/tasks.py
@@ -0,0 +1,27 @@
+# Задача 44: В ячейке ниже представлен код генерирующий DataFrame, которая состоит всего 
+# из 1 столбца. Ваша задача перевести его в one hot вид. Сможете ли вы это сделать без 
+# get_dummies? 
+  
+# import random 
+# lst = ['robot'] * 10 
+# lst += ['human'] * 10 
+# random.shuffle(lst) 
+# data = pd.DataFrame({'whoAmI':lst}) 
+# data.head() 
+  
+  
+import pandas as pd 
+  
+import random 
+lst = ['robot'] * 10 
+lst += ['human'] * 10 
+random.shuffle(lst) 
+data = pd.DataFrame({'whoAmI':lst}) 
+data.head(10) 
+  
+# Метод get_dummies 
+pd.get_dummies(data['whoAmI']).head(10) 
+  
+# Без метода get_dummies 
+pd.DataFrame({'human': data['whoAmI'] == 'human', 
+               'robot': data['whoAmI'] == 'robot'}).astype(int).head(10)
